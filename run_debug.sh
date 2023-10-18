e48599aa83180bb9c8dd6abf15202e81799bd452
Max Weber <mweber@kzvnr.de>
2023-10-18 09:25:07 +0200

    fixed flatpaks and gnome terminal config

--- run_debug.sh
+++ run_debug.sh
@@ -17,22 +17,10 @@ if ! [ -x "$(command -v pip)" ]; then
 fi
 sudo apt-get install python3-psutil
 
-if ! [[ -f "$HOME/.ssh/id_rsa" ]]; then
-    echo "Generating SSH Key"
-    mkdir -p "$HOME/.ssh"
-    ssh-keygen -b 4096 -t rsa -f "$HOME/.ssh/id_rsa" -N "" -C "$USER@$HOSTNAME"
-    echo "Publishing Key to authorized keys"
-    cat "$HOME/.ssh/id_rsa.pub" >> "$HOME/.ssh/authorized_keys"
-    chmod 600 "~/.ssh/authorized_keys"
-    echo "done"
-else
-    echo "SSH already generated"
-fi
-
 if [[ -f "./requirements.yml" ]]; then
     ansible-galaxy install -r requirements.yml
 fi
 
 ANSIBLE_LOCALHOST_WARNING=False \
 ANSIBLE_INVENTORY_UNPARSED_WARNING=False \
-ansible-playbook playbook.yml --ask-become-pass
+ansible-playbook tasks/debug.yml
